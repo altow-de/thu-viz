3ecd88e98e27585d0577d719afa2b3002c13f6ac
laurawolter <laura.wolter@posteo.de>
2024-01-04 10:30:19 +0100

    added page structure to project

---
+++ src/components/Content.tsx
@@ -0,0 +1,14 @@
+interface ContentProps {
+  children?: any;
+  title: string;
+}
+
+const Content = ({ children, title }: ContentProps) => {
+  return (
+    <div className="flex p-5 sm:p-8 bg-danube-100 rounded-b-md">
+      <div>{title}</div>
+      <div>{children}</div>
+    </div>
+  );
+};
+export default Content;

92659edb0dd79b6b25c24064b9f18aaa31879d34
laurawolter <laura.wolter@posteo.de>
2024-01-04 12:17:13 +0100

    small styling fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ import React, { useState } from "react";
 export default function Home() {
   const [selectedNav, setSelectedNav] = useState(0);
   return (
-    <main className="min-h-screen flex justify-center bg-gradient-to-b from-danube-200 to-danube-800 relative py-8 sm:py-16">
-      <div className="max-w-5xl h-full flex-1 bg-white rounded-md shadow-md">
+    <main className="min-h-screen flex justify-center bg-gradient-to-b from-danube-200 to-danube-800 relative py-8 sm:py-16 text-danube-950">
+      <div className="max-w-5xl h-full flex-1 bg-white rounded-lg shadow-md">
         <Header selectedNav={selectedNav} setSelectedNav={setSelectedNav} />
         <Content title={NavigationPage[selectedNav]} />
       </div>

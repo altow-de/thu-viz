96bd80553139000c24916793d19f1e43a6e4baed
laurawolter <laura.wolter@posteo.de>
2024-01-08 14:02:49 +0100

    layer for map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useRef, useState } from "react";
-import { Map, NavigationControl } from "maplibre-gl";
+import { Map, NavigationControl, Popup } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import MapStyle from "./MapStyle";
-import { MapStyles } from "@/frontend/constants";
+import { LayerZoom, MapStyles } from "@/frontend/constants";
 
-const OceanMap = () => {
+interface OceanMapProps {
+  type: string;
+}
+
+const OceanMap = ({ type = "route" }: OceanMapProps) => {
   const mapContainer = useRef(null);
   const map = useRef<Map | null>(null);
   const [mapStyle, setMapStyle] = useState(Object.keys(MapStyles)[0]);
@@ -16,21 +20,58 @@ const OceanMap = () => {
   };
 
   const onMapStyleChange = (mapStyle: string) => {
+    console.log(mapStyle);
+
     const mapUrl =
       "https://api.maptiler.com/maps/" + mapStyle + "/style.json?key=" + process.env.NEXT_PUBLIC_MAPTILER_ACCESS_TOKEN;
+
     map.current?.setStyle(`${mapUrl}`);
     map.current?.on("styledata", function () {
       handleSource();
       handleLayer();
+      handlePopUps();
     });
     setMapStyle(mapStyle);
+  };
+
+  const handlePopUps = () => {
+    // Create a popup, but don't add it to the map yet.
+    const popup = new Popup({
+      closeButton: false,
+      closeOnClick: false,
+    });
+
+    if (!map.current) return;
+    map.current.on("mouseenter", "route-point", (e) => {
+      // Change the cursor style as a UI indicator.
+      if (!map.current) return;
+      map.current.getCanvas().style.cursor = "pointer";
+      const coordinates = e?.features?.[0].geometry?.coordinates?.slice();
+      const html = `  <div>Hover info</div>  `;
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      // Populate the popup and set its coordinates
+      // based on the feature found.
+      popup.setLngLat(coordinates).setHTML(html).addTo(map.current);
+    });
+
+    map.current.on("mouseleave", "route-point", () => {
+      if (!map.current) return;
+      map.current.getCanvas().style.cursor = "";
+      popup.remove();
+    });
   };
 
   /**
    * Function to handle adding the map source.
    * @function
    */
-  const handleSource = () => {
+  const handleSource = async () => {
     if (!map.current?.getSource("openseamap-source")) {
       map.current?.addSource("openseamap-source", {
         type: "raster",
@@ -38,7 +79,81 @@ const OceanMap = () => {
         tileSize: 256,
       });
     }
+    if (type === "route") {
+      addRouteSource([
+        [12.08402, 54.1767],
+        [12.05402, 54.1867],
+        [11.00402, 54.2],
+        [11.50017, 54.77662],
+      ]);
+    }
   };
+
+  const addRouteSource = (coordinates: number[][]) => {
+    if (!map?.current?.getSource("route-source")) {
+      map.current?.addSource("route-source", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: coordinates,
+          },
+        },
+      });
+    }
+    if (!map?.current?.getSource("route-point-source")) {
+      const features = coordinates.map((coord) => {
+        return {
+          geometry: {
+            type: "Point",
+            coordinates: coord,
+          },
+          type: "Feature",
+        };
+      });
+      map.current?.addSource("route-point-source", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: features,
+        },
+      });
+    }
+  };
+
+  const addRouteLayer = () => {
+    //Route Layer
+    if (!map.current?.getLayer("route") && map.current?.getSource("route-source")) {
+      map.current?.addLayer({
+        ...LayerZoom,
+        id: "route",
+        type: "line",
+        source: "route-source",
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "#963748",
+          "line-width": 4,
+        },
+      });
+    }
+    //Point Layer
+    if (!map.current?.getLayer("route-point") && map.current?.getSource("route-point-source")) {
+      map.current?.addLayer({
+        id: "route-point",
+        type: "symbol",
+        source: "route-point-source",
+        layout: {
+          "icon-image": "circle",
+        },
+      });
+    }
+  };
+
   /**
    * Function to handle adding the map layer.
    * @function
@@ -49,12 +164,16 @@ const OceanMap = () => {
         id: "openseamap",
         type: "raster",
         source: "openseamap-source",
-        minzoom: 0,
-        maxzoom: 22,
+        ...LayerZoom,
         paint: {
           "raster-opacity": 0.8,
         },
       });
+    if (type === "route") {
+      addRouteLayer();
+    }
+    if (type === "marker") {
+    }
   };
 
   // Effect to initialize the map
@@ -68,9 +187,14 @@ const OceanMap = () => {
       center: [initialState.lng, initialState.lat],
       zoom: initialState.zoom,
     });
-    map.current.on("load", function () {
+    map.current?.loadImage("circle.png", (error, image) => {
+      if (error || !image) throw error;
+      map.current?.addImage("circle", image);
+    });
+    map.current.on("load", async function () {
       handleSource();
       handleLayer();
+      handlePopUps();
     });
     map.current.addControl(new NavigationControl(), "bottom-right");
   }, [mapStyle]);
@@ -78,7 +202,7 @@ const OceanMap = () => {
   return (
     <div className="relative">
       <MapStyle selectedStyle={mapStyle} setSelectedStyle={onMapStyleChange} />
-      <div className="h-[580px]" ref={mapContainer} />
+      <div className="h-[320px] sm:h-[580px]" ref={mapContainer} />
     </div>
   );
 };
--- src/frontend/constants.ts
+++ src/frontend/constants.ts
@@ -1,3 +1,5 @@
 export const NavigationPage = ["Overview", "Measurement Data"];
 
 export const MapStyles: { [key: string]: string } = { "basic-v2": "Basic", satellite: "Satellite" };
+
+export const LayerZoom = { minzoom: 0, maxzoom: 22 };

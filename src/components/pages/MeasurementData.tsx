53cf639f941a2dec598a9b754ef30a82767f5595
laurawolter <laura.wolter@posteo.de>
2024-01-16 14:05:46 +0100

    styling fix

--- src/components/pages/MeasurementData.tsx
+++ src/components/pages/MeasurementData.tsx
@@ -12,11 +12,13 @@ const MeasurementData = () => {
     <div className="">
       <AnkerMenu ankers={MeasurementAnkers} />
       <CardWraper text="Parameter over time" hasMap={false} id="parameter-over-time">
-        <Chart width={300} height={300} tickValue={100} x={"time"} y={"pressure"} title={"Pressure(mbar)"} />
-        <Chart width={300} height={300} tickValue={40} x={"time"} y={"temperature"} title={"Temperature(C)"} />
-        <Chart width={300} height={300} tickValue={20} x={"time"} y={"conductivity"} title={"Conductivity(ms/cm)"} />
+        <div>
+          <Chart width={300} height={300} tickValue={100} x={"time"} y={"pressure"} title={"Pressure(mbar)"} />
+          <Chart width={300} height={300} tickValue={40} x={"time"} y={"temperature"} title={"Temperature(C)"} />
+          <Chart width={300} height={300} tickValue={20} x={"time"} y={"conductivity"} title={"Conductivity(ms/cm)"} />
+        </div>
       </CardWraper>
-      <CardWraper text="Parameter over depths" hasMap={false} id="parameter-over-depths">
+      <CardWraper text="Parameter over depths" hasMap={false} id="parameter-over-depth">
         <Chart width={300} height={300} tickValue={100} x={"time"} y={"pressure"} title={"Pressure(mbar)"} />
       </CardWraper>
       <CardWraper text={"Tracks"} hasMap={true} id="track">

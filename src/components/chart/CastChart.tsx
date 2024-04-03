fbf107f35ba8e79d7a9db1b9ae75f113c94a851a
laurawolter <laura.wolter@posteo.de>
2024-04-03 12:22:18 +0200

    fixed mobile y brush

--- src/components/chart/CastChart.tsx
+++ src/components/chart/CastChart.tsx
@@ -293,11 +293,7 @@ const CastChart = ({
   return (
     <div id="chartContainer" className="flex-auto inline-block">
       <div className="pl-1 text-sm text-danube-600 font-semibold">{title}</div>
-      <svg
-        id={title + "-cast_chart"}
-        ref={d3Container}
-        style={{ display: "block", margin: "auto", touchAction: "none" }}
-      ></svg>
+      <svg id={title + "-cast_chart"} ref={d3Container} style={{ display: "block", margin: "auto" }}></svg>
     </div>
   );
 };
